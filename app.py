254955bdc1836dde201c7e9adab9da20934f406e
Zizou_8.T <ztesfa88@gmail.com>
2026-05-14 12:22:50 -0700

    Update Flask startup to bind 0.0.0.0 and use Render PORT

--- app.py
+++ app.py
@@ -339,4 +339,7 @@ def compare_files():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    host = os.environ.get("HOST", "0.0.0.0")
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host=host, port=port, debug=debug, use_reloader=False)
